150c4087900b57176661ce02ed3db5b6703f0423
karkran <nikhil.karkra@anz.com>
2018-12-01 13:55:04 +1100

    creating resolver

--- index.js
+++ index.js
@@ -1,45 +1,15 @@
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import schema from './schema';
-
+import resolvers from './resolvers'
 
 const app = express()
 
 app.get('/', (req, res)=>{
     res.send('GraphQL is amazing')
 })
+const root = resolvers
 
-class Friend {
-    constructor(id, {firstName, lastName, gender, language, email}){
-        this.id = id
-        this.firstName = firstName
-        this.lastName = lastName
-        this.gender = gender
-        this.language = language
-        this.email = email
-    }
-}
-
-const friendDatabase ={}
-
-const root ={
-    friend:()=>{
-        return {
-            "id":7857868,
-            "firstName":'Nikhil',
-            "lastName":'karkra',
-            "gender":'Male',
-            "language":'ENglish',
-            "emails":'nik@gmail.com'
-        }
-    },
-    createFriend:({input}) =>{
-        console.log(input)
-        let id  = require('crypto').randomBytes(10).toString('hex')
-        friendDatabase[id]= input
-        return new Friend(id, input)
-    }
-};
 app.use('/graphql', graphqlHTTP({
     schema:schema,
     rootValue:root,
